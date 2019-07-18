440721589af9e333badaf50c68e04d563678dbe1
PankajKrModi <pk.modi1433@gmail.com>
2019-07-18 09:41:23 +0530

    External api call

--- movie-info-service/src/main/java/io/mymicroservice/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/io/mymicroservice/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,10 +2,20 @@ package io.mymicroservice.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieInfoServiceApplication {
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
 	}
--- movie-info-service/src/main/java/io/mymicroservice/movieinfoservice/resources/MovieResources.java
+++ movie-info-service/src/main/java/io/mymicroservice/movieinfoservice/resources/MovieResources.java
@@ -1,17 +1,30 @@
 package io.mymicroservice.movieinfoservice.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import io.mymicroservice.movieinfoservice.models.Movie;
+import io.mymicroservice.movieinfoservice.models.MovieSummary;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResources {
 	
+	@Value("${api.key}")
+	private String apiKey;
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@RequestMapping("/{movieId}")
-	public Movie getMovie(@PathVariable("movieId") String movieId) {
-		return new Movie(movieId,"Transformer","hollywood ovie");
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		MovieSummary movieSummary = restTemplate.getForObject(
+				"https://api.themoviedb.org/3/movie/110?api_key=0d3c8b60df0e3710ad0b4ee456b1809f", MovieSummary.class);
+//				"https://api.themoviedb.org/3/movie/"+movieId+"?api_key="+apiKey, MovieSummary.class);
+		return new Movie(movieId,movieSummary.getTitle(),movieSummary.getOverview());
 	}
 }
