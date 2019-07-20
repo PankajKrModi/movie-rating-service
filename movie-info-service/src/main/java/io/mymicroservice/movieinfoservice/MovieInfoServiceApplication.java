c9e81ce2398f0593a37c775863f1c348fe01d50f
PankajKrModi <pk.modi1433@gmail.com>
2019-07-20 23:59:31 +0530

    added load balancer

--- movie-info-service/src/main/java/io/mymicroservice/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/io/mymicroservice/movieinfoservice/MovieInfoServiceApplication.java
@@ -8,10 +8,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieInfoServiceApplication {
 
-	@Bean
 	@LoadBalanced
+	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
--- movie-info-service/src/main/java/io/mymicroservice/movieinfoservice/resources/MovieResources.java
+++ movie-info-service/src/main/java/io/mymicroservice/movieinfoservice/resources/MovieResources.java
@@ -2,6 +2,7 @@ package io.mymicroservice.movieinfoservice.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ public class MovieResources {
 	@RequestMapping("/{movieId}")
 	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
 		MovieSummary movieSummary = restTemplate.getForObject(
-				"https://api.themoviedb.org/3/movie/110?api_key=0d3c8b60df0e3710ad0b4ee456b1809f", MovieSummary.class);
+				"https://api.themoviedb.org/3/movie/"+movieId+"?api_key="+apiKey, MovieSummary.class);
 //				"https://api.themoviedb.org/3/movie/"+movieId+"?api_key="+apiKey, MovieSummary.class);
 		return new Movie(movieId,movieSummary.getTitle(),movieSummary.getOverview());
 	}
